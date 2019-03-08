2b6d10344cd7c42dabccd196c39aa94cf2a01ff6
Tom Hulton-Harrop <tom.hultonharrop@gmail.com>
2019-03-08 22:46:55 +0000

    add regular library with usage

---
+++ regular/application/main.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+
+#include "calculator-lib/calculator.h"
+
+int main(int argc, char** argv)
+{
+    std::cout << square(4) << "\n";
+    return 0;
+}
---
+++ regular/library/src/calculator.cpp
@@ -0,0 +1,26 @@
+#include "calculator.h"
+
+int64_t add(int64_t lhs, int64_t rhs)
+{
+    return lhs + rhs;
+}
+
+int64_t substract(int64_t lhs, int64_t rhs)
+{
+    return lhs - rhs;
+}
+
+int64_t multiply(int64_t lhs, int64_t rhs)
+{
+    return lhs * rhs;
+}
+
+int64_t divide(int64_t lhs, int64_t rhs)
+{
+    return lhs / rhs;
+}
+
+int64_t square(int64_t value)
+{
+    return value * value;
+}
