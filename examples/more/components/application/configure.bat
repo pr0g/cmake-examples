14082b0d60ee8d3f25c2f2d34fb42e0b055dd75d
Tom Hulton-Harrop <tom.hultonharrop@gmail.com>
2020-04-10 12:10:20 +0100

    remove accidentally included absolute path

--- examples/more/components/application/configure.bat
+++ examples/more/components/application/configure.bat
@@ -1,3 +1,3 @@
 @echo off
 
-cmake -S . -B build -DCMAKE_PREFIX_PATH=%cd%/../library/install;C:\Users\tomhultonharrop\Documents\fmt\install
+cmake -S . -B build -DCMAKE_PREFIX_PATH=%cd%/../library/install
