3cc221da1d119992059ccfccb6be646b74d43ada
Alexandru Smeu <alexandru.smeu@pss.ro>
2020-10-06 02:05:05 +0300

    A bit more complex example using ExternalProject_Add to fetch external libraries
    catch2, rapidjson -> header only
    restbed -> used as a static library
    
    All have CMake support
    
    1. `restbed` does not have a FindRestbed.cmake or a proper config module (at least to my checks)
    2. `catch2` and `rapidjson` work with FetchProject but I wanted a consistent way of getting the libraries and setting them up

---
+++ examples/more/external-project-add/cmake/Restbed_External.cmake
@@ -0,0 +1,48 @@
+include(ExternalProject)
+
+###############################################################################
+# restbed 4.7
+###############################################################################
+# The parent script needs to define the "GLOBAL_OUTPUT_PATH" variable,
+# which will be used as output directory for all *.lib, *.dll, *.a, *.so, *.pdb files.
+
+set(RESTBED_ROOT        ${GLOBAL_OUTPUT_PATH}/restbed)
+set(RESTBED_LIB_DIR     ${RESTBED_ROOT}/library)
+set(RESTBED_INCLUDE_DIR ${RESTBED_ROOT}/include)
+
+ExternalProject_Add(
+    restbed_external
+
+    GIT_REPOSITORY "https://github.com/Corvusoft/restbed"
+    GIT_TAG "4.7"
+    GIT_PROGRESS TRUE
+
+    SOURCE_DIR "${CMAKE_SOURCE_DIR}/3rdparty/restbed"
+    CMAKE_ARGS 
+        -DBUILDSHARED=OFF 
+        -DBUILD_TESTS=OFF 
+        -DBUILD_SSL=OFF 
+        -DBUILD_SHARED_LIBS=OFF 
+        -DCMAKE_INSTALL_PREFIX=${RESTBED_ROOT}
+        -DCMAKE_PREFIX_PATH=${RESTBED_ROOT}
+
+    TEST_COMMAND  ""
+    UPDATE_COMMAND ""
+    PATCH_COMMAND  ""
+
+    INSTALL_DIR ${GLOBAL_OUTPUT_PATH}/restbed
+)
+
+# Hack to make it work, otherwise INTERFACE_INCLUDE_DIRECTORIES will not be propagated
+# https://stackoverflow.com/questions/45516209/cmake-how-to-use-interface-include-directories-with-externalproject
+file(MAKE_DIRECTORY ${RESTBED_ROOT})
+file(MAKE_DIRECTORY ${RESTBED_LIB_DIR})
+file(MAKE_DIRECTORY ${RESTBED_INCLUDE_DIR})
+
+add_library(restbed STATIC IMPORTED)
+set_target_properties(restbed PROPERTIES 
+    IMPORTED_LOCATION "${RESTBED_LIB_DIR}/${CMAKE_STATIC_LIBRARY_PREFIX}restbed${CMAKE_STATIC_LIBRARY_SUFFIX}"
+    INTERFACE_INCLUDE_DIRECTORIES ${RESTBED_INCLUDE_DIR}
+)
+
+add_dependencies(restbed restbed_external)
