5deaf3500ec16f9b315b634824acaf5d9f85f970
Alexandru Smeu <alexandru.smeu@pss.ro>
2020-10-06 14:46:46 +0300

    Handle multiple configs

--- examples/more/external-project-add/cmake/Restbed_External.cmake
+++ examples/more/external-project-add/cmake/Restbed_External.cmake
@@ -13,12 +13,15 @@ set(RESTBED_INCLUDE_DIR ${RESTBED_ROOT}/include)
 
 message(INFO "ExternalProject_Add() - ${EXTERNAL_LIB} - building ${CMAKE_BUILD_TYPE}")
 
+# Long discussion on how to properly handly cross platform builds
+# with single-target / multiple-target compilers
+# https://gitlab.kitware.com/cmake/cmake/-/issues/17645
 ExternalProject_Add(
     restbed_external
 
     GIT_REPOSITORY "https://github.com/Corvusoft/restbed"
-    GIT_TAG "4.7"
-    GIT_PROGRESS TRUE
+    GIT_TAG        "4.7"
+    GIT_PROGRESS   TRUE
 
     SOURCE_DIR "${CMAKE_SOURCE_DIR}/3rdparty/restbed"
     CMAKE_ARGS 
@@ -30,11 +33,12 @@ ExternalProject_Add(
         -DCMAKE_PREFIX_PATH=${RESTBED_ROOT}
         -DCMAKE_BUILD_TYPE=${CMAKE_BUILD_TYPE}
 
-    TEST_COMMAND  ""
+    TEST_COMMAND   ""
     UPDATE_COMMAND ""
     PATCH_COMMAND  ""
 
-    INSTALL_DIR ${GLOBAL_OUTPUT_PATH}/restbed
+    INSTALL_DIR     ${GLOBAL_OUTPUT_PATH}/restbed
+    INSTALL_COMMAND ${CMAKE_COMMAND} --build . --config $<CONFIG> --target install
 )
 
 # Hack to make it work, otherwise INTERFACE_INCLUDE_DIRECTORIES will not be propagated
