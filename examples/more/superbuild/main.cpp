a8a384c8cc165a6bc3758039e8d7e249a701c967
Tom Hulton-Harrop <tom.hultonharrop@gmail.com>
2022-10-23 17:37:08 -0700

    improve sample application very slightly

--- examples/more/superbuild/main.cpp
+++ examples/more/superbuild/main.cpp
@@ -1,5 +1,7 @@
 #include <SDL.h>
 
+#include <cstdio>
+
 int main(int argc, char** argv)
 {
   if (SDL_Init(SDL_INIT_VIDEO) < 0) {
@@ -18,6 +20,14 @@ int main(int argc, char** argv)
     return 1;
   }
 
+  SDL_Renderer* renderer = SDL_CreateRenderer(
+    window, -1, SDL_RENDERER_ACCELERATED | SDL_RENDERER_PRESENTVSYNC);
+  
+  if (renderer == nullptr) {
+    printf("Renderer could not be created. SDL_Error: %s\n", SDL_GetError());
+    return 1;
+  }
+
   for (bool running = true; running;) {
     for (SDL_Event currentEvent; SDL_PollEvent(&currentEvent) != 0;) {
       if (currentEvent.type == SDL_QUIT) {
@@ -25,8 +35,13 @@ int main(int argc, char** argv)
         break;
       }
     }
+    // cornflower blue
+    SDL_SetRenderDrawColor(renderer, 100, 149, 237, 255);
+    SDL_RenderClear(renderer);
+    SDL_RenderPresent(renderer);
   }
 
+  SDL_DestroyRenderer(renderer);
   SDL_DestroyWindow(window);
   SDL_Quit();
 
